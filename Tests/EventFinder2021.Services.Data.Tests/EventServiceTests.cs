c80f37c496d92f675394b95edc2a62f8833f5999
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-07-15 23:14:41 +0300

    Added more tests for EventServiceTests

--- Tests/EventFinder2021.Services.Data.Tests/EventServiceTests.cs
+++ Tests/EventFinder2021.Services.Data.Tests/EventServiceTests.cs
@@ -17,29 +17,21 @@
 
     public class EventServiceTests
     {
+        private readonly CreateEventInputModel inputModel;
+        private readonly ApplicationUser user;
 
-
-        [Fact]
-        public async Task CreateEventSuccsefullyCreatesEvent()
+        public EventServiceTests()
         {
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase("test");
-
-            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
-            var voteserivce = new VoteService(dbContext);
-            var service = new EventService(dbContext, voteserivce);
-
-            var user = new ApplicationUser()
+            this.user = new ApplicationUser()
             {
-                Id = "user",
+                UserName = "User",
             };
-
-            var currEvent = new CreateEventInputModel()
+            this.inputModel = new CreateEventInputModel()
             {
                 Category = (EventFinder2021.Data.Models.Enums.Category)1,
                 City = (EventFinder2021.Data.Models.Enums.City)1,
                 Description = "aaaaaaaaaaaaaaaa",
-                CreatedByuser = "user",
+                CreatedByuser = "User",
                 Name = "Name",
                 Date = DateTime.Now,
             };
@@ -58,58 +50,80 @@
             fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
             fileMock.Setup(_ => _.FileName).Returns(fileName);
             fileMock.Setup(_ => _.Length).Returns(ms.Length);
-            currEvent.Image = fileMock.Object;
+            this.inputModel.Image = fileMock.Object;
+        }
 
-            await service.CreateEventAsync(currEvent, "ss");
+        [Fact]
+        public async Task CreateEventSuccsefullyCreatesEvent()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("CreateEventTest");
 
-            var eventById = service.GetEventById(1);
-            var events = service.GetAllEvents(1, 12);
-            Assert.Equal(currEvent.Name, eventById.Name);
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+            var voteserivce = new VoteService(dbContext);
+            var service = new EventService(dbContext, voteserivce);
+
+            await service.CreateEventAsync(this.inputModel, "ss");
 
             var count = service.GetCount();
-            Assert.Equal(1, count);          
+            Assert.Equal(1, count);
         }
 
         [Fact]
         public async Task WhenDeleteEventNoLongerVisible()
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase("test");
+            .UseInMemoryDatabase("DeleteEventTest");
 
             var dbContext = new ApplicationDbContext(optionsBuilder.Options);
             var voteserivce = new VoteService(dbContext);
             var service = new EventService(dbContext, voteserivce);
 
-            var currEvent = new CreateEventInputModel()
-            {
-                Category = (EventFinder2021.Data.Models.Enums.Category)1,
-                City = (EventFinder2021.Data.Models.Enums.City)1,
-                Description = "aaaaaaaaaaaaaaaa",
-                CreatedByuser = "user",
-                Name = "Name",
-                Date = DateTime.Now,
-            };
-
-            // Arrange
-            var fileMock = new Mock<IFormFile>();
-
-            // Setup mock file using a memory stream
-            var content = "Hello World from a Fake File";
-            var fileName = "test.pdf";
-            var ms = new MemoryStream();
-            var writer = new StreamWriter(ms);
-            writer.Write(content);
-            writer.Flush();
-            ms.Position = 0;
-            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
-            fileMock.Setup(_ => _.FileName).Returns(fileName);
-            fileMock.Setup(_ => _.Length).Returns(ms.Length);
-            currEvent.Image = fileMock.Object;
-
-            await service.CreateEventAsync(currEvent, "ss");
+            await service.CreateEventAsync(this.inputModel, "ss");
             await service.DeleteEvent(1);
             var count = service.GetCount();
             Assert.Equal(0, count);
+        }
+
+        [Fact]
+
+        public async Task WhenCallUserEventsReturnUserEvents()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+    .UseInMemoryDatabase("UserEventTest");
+
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+            var voteserivce = new VoteService(dbContext);
+            var service = new EventService(dbContext, voteserivce);
+
+            dbContext.Users.Add(this.user);
+            await dbContext.SaveChangesAsync();
+            await service.CreateEventAsync(this.inputModel, "ss");
+
+            var userS = service.GetAllEvents(1, 12);
+            var userEvents = service.GetEventsByUser("User").ToList();
+            //TO FIX
+            Assert.Equal(this.user.Events.Count(), userEvents.Count());
+        }
+
+        [Fact]
+
+        public async Task WhenCallGetAllEventsReturnsAllEvents()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+    .UseInMemoryDatabase("ReturnAllEventsTest");
+
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+            var voteserivce = new VoteService(dbContext);
+            var service = new EventService(dbContext, voteserivce);
+
+            await service.CreateEventAsync(this.inputModel, "ss");
+            await service.CreateEventAsync(this.inputModel, "ss");
+            await service.CreateEventAsync(this.inputModel, "ss");
+
+            var allEvents = service.GetAllEvents(1, 12);
+
+            Assert.Equal(3, allEvents.Count());
         }
     }
 }
