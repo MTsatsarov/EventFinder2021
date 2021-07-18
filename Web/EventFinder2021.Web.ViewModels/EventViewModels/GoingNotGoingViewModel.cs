cafc7ba44d239b3508a0167954445af08c60c5bd
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-07-18 11:21:51 +0300

    AddedViewModelGoingUsers

---
+++ Web/EventFinder2021.Web.ViewModels/EventViewModels/GoingNotGoingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EventFinder2021.Web.ViewModels.EventViewModels
+{
+    using Newtonsoft.Json;
+
+    public class GoingNotGoingViewModel
+    {
+        [JsonProperty(PropertyName = "goingUsersCount")]
+        public int GoingUsersCount { get; set; }
+
+        [JsonProperty(PropertyName = "notGoingUsersCount")]
+        public int NotGoingUsersCount { get; set; }
+    }
+}
