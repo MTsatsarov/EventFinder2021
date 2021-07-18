171e7923c2f75a223af9fb9f6645ab4789ff6f05
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-07-18 11:29:16 +0300

    Fix Votes

--- Web/EventFinder2021.Web.ViewModels/VotesModel/VoteViewModel.cs
+++ Web/EventFinder2021.Web.ViewModels/VotesModel/VoteViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace EventFinder2021.Web.ViewModels.VotesModel
 {
+    using Newtonsoft.Json;
+
     public class VoteViewModel
     {
+        [JsonProperty(PropertyName = "averageVoteValue")]
         public double AverageVoteValue { get; set; }
     }
 }
