04a16b59b0fb376de16d054040d8ee11c72732ca
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-08-01 15:06:22 +0300

    Ui update (hover effect on /Index) stylish search button

---
+++ Web/EventFinder2021.Web/wwwroot/js/HomeIndex.js
@@ -0,0 +1,29 @@
+﻿function Hover() {
+
+    [...document.getElementsByName('eventCards')].forEach(x => {
+        x.addEventListener('mouseover', onHover);
+        x.addEventListener('mouseout', onMouseOut)
+    })
+}
+function onHover(ev) {
+    var element = ev.target;
+    if (element.tagName == 'IMG' || element.tagName == 'A' || element.tagName == 'P') {
+        element.parentNode.parentNode.setAttribute('style', 'background-color:gray;');
+    }
+    else {
+        element.parentNode.setAttribute('style', 'background-color:gray;');
+    }
+
+
+
+}
+function onMouseOut(ev) {
+    var element = ev.target;
+    if (element.tagName == 'IMG' || element.tagName == 'A' || element.tagName == 'P') {
+        element.parentNode.parentNode.setAttribute('style', 'background-color:none');
+    }
+    else {
+        element.parentNode.setAttribute('style', 'background-color:none');
+    }
+
+}
